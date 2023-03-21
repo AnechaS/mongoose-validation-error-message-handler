ede038236f7a994e4834b5ef69f575b32b1c2cc6
AnechaS <Anachitheu.k@gmail.com>
2023-03-21 14:18:15 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,35 +1,34 @@
 type ValidatorMessages = {
-  base: string;
-  required: string;
-  enum: string;
-  validate: string;
-  unique: string;
-  buffer: string;
-  boolean: string;
-  objectId: string;
-  map: string;
-  string: string;
-  maxlength: string;
-  minlength: string;
-  regexp: string;
-  number: string;
-  'number.max': string;
-  'number.min': string;
-  date: string;
-  'date.max': string;
-  'date.min': string;
+  base?: string;
+  required?: string;
+  enum?: string;
+  validate?: string;
+  unique?: string;
+  buffer?: string;
+  boolean?: string;
+  objectId?: string;
+  map?: string;
+  string?: string;
+  maxlength?: string;
+  minlength?: string;
+  regexp?: string;
+  number?: string;
+  'number.max'?: string;
+  'number.min'?: string;
+  date?: string;
+  'date.max'?: string;
+  'date.min'?: string;
 };
 
 type Paths = {
-  [path: string]: { origin: Boolean; kind: string; message: string };
+  [path: string]: {
+    origin?: Boolean;
+    kind?: string;
+    message?: string;
+  };
 };
 
-type Options = {
-  messages?: ValidatorMessages;
-  paths?: Paths;
-};
-
-declare function mongooseValidationErrorHandler(error: Error, options?: Options): Error;
+declare function mongooseValidationErrorHandler(error: Error, options?: { messages?: ValidatorMessages, paths?: Paths }): Error;
 declare namespace mongooseValidationErrorHandler {}
 
 export = mongooseValidationErrorHandler;
